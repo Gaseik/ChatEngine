09efc5d8753f9359d7cb5a8ba91fcc8eaa8cde79
James Chen <james.chen@cefinity.com>
2021-03-12 10:20:48 +0800

    components added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { ChatEngine } from 'react-chat-engine'
 import './App.css'
+import ChatFeed from './components/ChatFeed'
 
 
-
-cosnt App = () => {
+const App = () => {
     return (
         <ChatEngine
             height='100vh'
             projectID="dfd2db7f-3b22-4fbf-96fd-93b1469c2a32"
             userName="Gaseik2699"
             userSecret="123"
-
-        
-        
+            renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps} />}
         />
     )
 }
---
+++ src/components/TheirMessage.jsx
@@ -0,0 +1,7 @@
+export default function TheirMessgae () {
+    return(
+        <div className="">
+            TheirMessgae
+        </div>
+    )
+}
